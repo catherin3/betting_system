d06673492c0772dbaaee1643735dbab855f1d14f
Boogle <boogle@Boogles-iMac.local>
2020-11-03 11:48:00 +0800

    changes

--- src/Components/Betting/BettingPage.tsx
+++ src/Components/Betting/BettingPage.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Typography, Grid, Select, MenuItem, FormControl, InputLabel , TextField} from '@material-ui/core'
+import { Typography, Grid, Select, MenuItem, FormControl, InputLabel, TextField, IconButton, Button, Paper } from '@material-ui/core'
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import NavBar from './NavBar'
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles(() =>
     createStyles({
@@ -13,10 +14,16 @@ const useStyles = makeStyles(() =>
 
 export default function BettingPage() {
     const classes = useStyles();
-    const [type, setType] = useState<number>()
+    const [type, setType] = useState<number>(0)
+    const [chosenNumber, setChosenNumber] = useState<number>(0)
     const [success, setSuccess] = useState<boolean>(false)
+    //const [numberArray, setNumberArray] = useState<Array<Number>>()
+    const numberArray:Number[] =[]
     const handleChange = (e: any) => setType(e.target.value)
-    const [numberArray, setNumberArray] = useState<Array<Number>>()
+    const handleNumber = (e: any) => setChosenNumber(e.target.value)
+    let disabled=false
+    const [text, setText] = useState<String>('')
+    //setNumberArray([])
     return (
         <div>
             <NavBar />
@@ -24,8 +31,8 @@ export default function BettingPage() {
                 <Grid item style={{ justifyContent: 'center', alignItems: 'center' }}>
                     <FormControl className={classes.formControl}>
                         <InputLabel>Type</InputLabel>
-                        <Select onChange={handleChange} >
-                            <MenuItem value={7}>     7</MenuItem>
+                        <Select onChange={handleChange} value={type}>
+                            <MenuItem value={7}>7</MenuItem>
                             <MenuItem value={8}>8</MenuItem>
                             <MenuItem value={9}>9</MenuItem>
                             <MenuItem value={10}>10</MenuItem>
@@ -38,11 +45,64 @@ export default function BettingPage() {
                     </FormControl>
                     <p />
                     <Typography>You Selected:{type}</Typography>
-                    <p/>
+                    <p />
                     <Typography>Pick {type} numbers from 1 to 52</Typography>
-                    <TextField type='number' InputProps={{ inputProps: { min: 1, max: 52 } }} style={{minWidth:120}}/>
+                    <TextField
+                        type='number'
+                        InputProps={{ inputProps: { min: 1, max: 52 } }}
+                        style={{ minWidth: 120 }}
+                        onChange={handleNumber}
+                        error={chosenNumber > 54 && chosenNumber < 0}
+                        helperText={type === 0 ? 'Empty field!' : chosenNumber}
+                      //  disabled={ numberArray.length === type ? false:true}
+                    >
+                    </TextField>
+                    <IconButton disabled={disabled} onClick={() => {
+                        if (numberArray.length < type) {
+                            if (numberArray && chosenNumber < 54 && chosenNumber > 0) {
+                                numberArray.push(chosenNumber)
+                                console.log(numberArray)
+                                if (numberArray.length === type) {
+                                    setSuccess(true)
+                                    disabled=true
+                                } 
+                            }
+                            else {  
+                                console.log('Error')
+                            }
+                        }
+                        else{
+                            setSuccess(true)
+                            disabled=true
+                        }
+
+                    }
+                    }><AddIcon /></IconButton>
+                    <Typography>Numbers chosen: {numberArray} </Typography>
+                    <Button onClick={() =>{
+                        if (success === true){
+                            setText('Successfully Placed your bets')
+                        }
+                        else{
+                            setText('Not Successful')
+                        }
+                        console.log(success)
+                        console.log(text)
+                    }}>Submit</Button>
+                    <Paper>
+                    <Typography>{text}</Typography>
+                    </Paper>
                 </Grid>
             </Grid>
         </div>
     )
 }
+
+
+/*
+
+while(numberArray.length<type){
+    if numberArray
+    numberArray.push(chosenNumber)
+}
+*/
